d41d876fb8b02528c011307fb5f3fd3f88cebc54
edyedy <edydavidgasser@gmail.com>
2022-07-14 20:10:17 +0200

    added code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './components/layout/Header.js'
 import Meals from './components/meals/Meals.js';
+import Cart from './components/cart/Cart.js';
 
 function App() {
   return (
     <React.Fragment>
+      <Cart/>
       <Header />
       <main>
         <Meals/>
